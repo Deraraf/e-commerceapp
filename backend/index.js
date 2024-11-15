73399327f8ef2147b29f725cd3a98f8156045b44
derara <derarageremu@gmail.com>
2024-11-15 08:18:42 +0300

    solve origin error

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ await connectedDB();
 const app = express();
 app.use(
   cors({
-    origin: ["https://e-commerce-app-api-two.vercel.app"],
+    origin: [""],
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
     credentials: true,
   })
