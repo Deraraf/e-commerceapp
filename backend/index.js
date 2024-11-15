46e4309e8d862af33a515fc3d5e2eac375ca029a
Derara <114152134+Deraraf@users.noreply.github.com>
2024-11-15 12:29:28 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ await connectedDB();
 const app = express();
 app.use(
   cors({
-    origin: ["e-commerce-app-frontend-orcin.vercel.app"],
+    origin: ["https://e-commerce-app-frontend-orcin.vercel.app"],
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
     credentials: true,
   })
