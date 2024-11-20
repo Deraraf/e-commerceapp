1fc49be53c1b9767ca9e031046796647c55e7ced
derara <derarageremu@gmail.com>
2024-11-20 19:44:13 +0300

    setup base url to env

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "";
+export const BASE_URL =
+  import.meta.env.VITE_BASE_URL || "https://e-commerce-app-apimern.vercel.app";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
