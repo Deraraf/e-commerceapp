22643e789cb0528c6064922d39e4decc5b8f3120
derara <derarageremu@gmail.com>
2024-11-20 19:03:58 +0300

    fiexed cloudinary error

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,10 +1,14 @@
-import { v2 as cloudinary } from "cloudinary";
+import pkg from "cloudinary";
 import dotenv from "dotenv";
+
 dotenv.config();
+
+const { v2: cloudinary } = pkg;
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Your Cloudinary cloud name
-  api_key: process.env.CLOUDINARY_API_KEY, // Your Cloudinary API key
-  api_secret: process.env.CLOUDINARY_API_SECRET, // Your Cloudinary API secret
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
