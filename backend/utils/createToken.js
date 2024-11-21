de8bdb3de25b0bf91eb8dd35a397b0a23876d3e8
derara <derarageremu@gmail.com>
2024-11-21 07:46:17 +0300

    also soved cookies

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,7 +7,7 @@ const createToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "production",
+    secure: process.env.NODE_ENV === "production",
     sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
